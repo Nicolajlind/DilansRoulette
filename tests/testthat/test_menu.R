66c83a9c50d3eab4c550ec520bf2accf8ba5265a
Nicolaj <nicolajlind@live.dk>
2025-04-06 15:42:29 +0200

    check for colnames

--- tests/testthat/test_menu.R
+++ tests/testthat/test_menu.R
@@ -2,6 +2,9 @@ testthat::test_that("Dilansmenu loads as expected", {
   testthat::expect_no_error(dilans_menu())
 
   menu <- dilans_menu()
+  expected_cols <- c("nummer", "kategori", "navn", "beskrivelse")
+  testthat::expect_named(menu, expected_cols)
+
   elementer <- nrow(menu)
   numre <- menu$nummer |> unique() |> length()
   testthat::expect_equal(elementer, numre)
