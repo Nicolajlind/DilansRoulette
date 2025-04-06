ca787659a31fb64e292d9dbb7a8e7252bd3be6aa
Nicolaj <nicolajlind@live.dk>
2025-04-06 15:13:27 +0200

    Fuldt menukort

--- tests/testthat/test_menu_desc.R
+++ tests/testthat/test_menu_desc.R
@@ -1,5 +1,5 @@
 testthat::test_that("menu_desc works as expected", {
-  testthat::expect_equal(menu_desc("50"), "NO: 50: DURUM GRÆSK BØF")
-  testthat::expect_equal(menu_desc("50.A"), "NO: 50.A: DURUM FALAFEL")
-  testthat::expect_equal(menu_desc("5"), "NO: 5: MARGHERITA - tomatsauce og ost")
+  testthat::expect_equal(menu_desc("50"), "NO: 50: Durum Ruller - GRÆSK BØF")
+  testthat::expect_equal(menu_desc("50.A"), "NO: 50.A: Durum Ruller - FALAFEL")
+  testthat::expect_equal(menu_desc("5"), "NO: 5: Pizza - MARGHERITA - tomatsauce og ost")
 })
